36ffe079571dcde56ca7fa31e49213e4bc3bd407
Gabriel Leandro <gabriel.leandrods@gmail.com>
2024-10-22 19:58:18 -0300

    adicionando dialog

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,14 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.interface';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { EDialogPainelClass } from '../../enum/EDialogPainelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
+
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+
+  #dialog = inject(MatDialog);
 
   public arrayProjects = signal<IProjects[]>([
     {
@@ -103,4 +109,10 @@ export class ProjectsComponent {
     },
   ]);
   
+  public openDialog(data: IProjects){
+    this.#dialog.open(DialogProjectsComponent , {
+      data,
+      panelClass: EDialogPainelClass.PROJECTS,
+    });
+  }
 }
