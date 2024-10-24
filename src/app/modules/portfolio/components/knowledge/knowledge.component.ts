26706dc2e2d05997f8357b9b424b46390532109f
Gabriel Leandro <gabriel.leandrods@gmail.com>
2024-10-24 17:52:35 -0300

    Mundança de cor + adicionando tooltip

--- src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -1,45 +1,54 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IKnowledge } from '../../interface/IKnowledge.interface';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-knowledge',
   standalone: true,
-  imports: [],
+  imports: [MatTooltipModule],
   templateUrl: './knowledge.component.html',
   styleUrl: './knowledge.component.scss'
 })
 export class KnowledgeComponent {
+
+  // #tooltip = inject(MatTooltipModule);
 
   //Com singal (ele fica observando os dados em tempo real)
   public arrayKnowledge = signal<IKnowledge[]>([
     {
       src: 'assets/icons/knowledge/angular.svg',
       alt: 'Ícone do Angular',
+      title: 'Angular',
     },
     {
       src: 'assets/icons/knowledge/css3.svg',
       alt: 'Ícone do CSS3',
+      title: 'CSS3',
     },
     {
       src: 'assets/icons/knowledge/dotnet.svg',
-      alt: 'Ícone do HTML5',
+      alt: 'Ícone do .NET',
+      title: '.NET',
     },
     {
       src: 'assets/icons/knowledge/html5.svg',
       alt: 'Ícone do HTML5',
+      title: 'HTML5',
     },
     {
       src: 'assets/icons/knowledge/javascript.svg',
       alt: 'Ícone do JavaScript',
+      title: 'JavaScript',
     },
     {
       src: 'assets/icons/knowledge/nodejs.svg',
       alt: 'Ícone do Node.js',
+      title: 'Node.js',
     },
     {
       src: 'assets/icons/knowledge/sass.svg',
       alt: 'Ícone do Sass',
+      title: 'Sass',
     }
   ])
-
 }
---
+++ src/app/modules/portfolio/enum/EToolTipClass.enum.ts
@@ -0,0 +1,3 @@
+export enum EToolTipClass {
+    KNOWLEDGE = 'tooltip-container-knowledge'
+}
